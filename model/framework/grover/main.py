29087e3a240cb8688300534c8bfef2315e03c926
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-10-05 23:41:31 +0200

    Update main.py

--- model/framework/grover/main.py
+++ model/framework/grover/main.py
@@ -45,7 +45,8 @@ def smiles_to_dataframe(txt_file_path):
     
     return input_csv_path
 
-    
+tmp_folder = tempfile.mktemp()
+features_path = os.path.join(tmp_folder, "features.npz")    
 
 if __name__ == '__main__':
     # setup random seed
@@ -67,10 +68,10 @@ if __name__ == '__main__':
     p = Path(s)
     model_path = str(p.parent.parent.absolute())
 
-    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/bbbp', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/bbbp/fold_0/model_0/model.pt', model_path+'/framework/finetune/bbbp/fold_2/model_0/model.pt', model_path+'/framework/finetune/bbbp/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=['features.npz'], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
+    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/bbbp', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/bbbp/fold_0/model_0/model.pt', model_path+'/framework/finetune/bbbp/fold_2/model_0/model.pt', model_path+'/framework/finetune/bbbp/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=[features_path], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
 
 
-    sf.save_features_main(csv_path)
+    sf.save_features_main(csv_path, features_path)
 
     train_args = get_newest_train_args()
     avg_preds, test_smiles = make_predictions(args, train_args)
